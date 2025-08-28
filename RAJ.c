36a5c11abc14c1ebade783b37b752034be0ee871
Raj <rajroshanbharti91@gmail.com>
2025-08-28 11:58:37 +0530

    Add files via upload

---
+++ RAJ.c
@@ -0,0 +1,18 @@
+//Develop by : Raj     Date : 26/08/2025
+#include <stdio.h>
+ void main ( )
+{
+    int num1, num2, sum;
+
+
+    printf("Enter two integers: \n");
+
+
+    scanf("%d %d", &num1, &num2);
+
+
+    sum = num1 + num2;
+
+
+    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+}
