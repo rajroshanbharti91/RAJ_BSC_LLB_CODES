cdb1f498f2b9cbf9e6d122d82530a7bfcfc59e53
Raj <rajroshanbharti91@gmail.com>
2025-09-17 15:32:09 +0530

    Add files via upload

---
+++ Raj.c
@@ -0,0 +1,14 @@
+//program to print first n natural number
+#include<stdio.h>
+void main ()
+{
+    int n,i;
+    printf("Enter the value of n\n" );
+    scanf("%d",&n);
+    for( i=1;i<=n;)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+
+}
